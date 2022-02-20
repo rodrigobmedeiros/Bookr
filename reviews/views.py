f3d14471a44e1f33b8df33d7c1f5ac27d9a19ac0
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-20 07:31:56 -0300

    remove code we dont need from index view

--- reviews/views.py
+++ reviews/views.py
@@ -4,10 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
 
-    name = request.GET.get('name') or 'World'
-
-    context = {
-        'name': name
-    }
-
-    return render(request, 'base.html', context)
+    return render(request, 'base.html')
