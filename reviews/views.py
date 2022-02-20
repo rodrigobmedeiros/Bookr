7de48f95ec8b6f5158985ecbf4882e363c602bc1
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-20 07:36:04 -0300

    create a search view to get a url term to search into database

--- reviews/views.py
+++ reviews/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 def index(request):
 
     return render(request, 'base.html')
+
+def book_search(request):
+
+    search_term = request.GET.get('search')
+    context = {'search_term': search_term}
+
+    return render(request, 'search.html', context=context)
