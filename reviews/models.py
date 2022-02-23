efeaa5964711ab4c2b07ac62142d524fd8b044d4
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-23 11:19:50 -0300

    include __str__ method in model classes

--- reviews/models.py
+++ reviews/models.py
@@ -8,6 +8,10 @@ class Publisher(models.Model):
     website = models.URLField(help_text="The Publisher's website.")
     email = models.EmailField(help_text="The Publisher's email address.")
 
+    def __str__(self):
+
+        return self.name
+
 class Book(models.Model):
     """A published book."""
     title = models.CharField(max_length=70, help_text='The title of the book.')
@@ -16,11 +20,19 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     contributors = models.ManyToManyField('Contributor', through='BookContributor')
 
+    def __str__(self):
+
+        return self.title
+
 class Contributor(models.Model):
     """A contributor to a Book, e.g. author, editor, co-author."""
     first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
     last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
+
+    def __init__(self):
+
+        return self.first_names
 
 class BookContributor(models.Model):
     """Estabilish many to many relationship between Books and Contributors"""
