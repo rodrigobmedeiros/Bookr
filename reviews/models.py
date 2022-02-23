6bf6bb0b42efff30cbda0ae5cd14992aa740a91b
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-23 04:52:50 -0300

    add Book model

--- reviews/models.py
+++ reviews/models.py
@@ -6,3 +6,10 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50, help_text="The name of the Publisher.")
     website = models.URLField(help_text="The Publisher's website.")
     email = models.EmailField(help_text="The Publisher's email address.")
+
+class Book(models.Model):
+    """A published book."""
+    title = models.CharField(max_length=70, help_text='The title of the book.')
+    publication_date = models.DateField(verbose_name='Date the book was published.')
+    isbn = models.CharField(max_length=20, verbose_name='ISBN number of the book.')
+
