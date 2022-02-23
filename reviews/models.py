59d88630c473b7587a7a129ea94df7151ff89182
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-23 04:53:30 -0300

    remove default django comments from models.py file

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Publisher(models.Model):
     """A company that publishes books."""
     name = models.CharField(max_length=50, help_text="The name of the Publisher.")
