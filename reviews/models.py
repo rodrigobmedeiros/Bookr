86ed2498f3f089231bac7ff8119f8f2f88328702
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-23 10:22:09 -0300

    add review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.db import models
 
 class Publisher(models.Model):
@@ -34,3 +35,12 @@ class BookContributor(models.Model):
         choices=ContributionRole.choices, 
         max_length=20
     )
+
+class Review(models.Model):
+    """Reviews added by users"""
+    content = models.TextField(help_text='The review text.')
+    rating = models.IntegerField(help_text='The rating the reviewer has given.')
+    date_created = models.DateTimeField(auto_now=True, help_text='The date and time the review was created.')
+    date_edited = models.DateTimeField(null=True, help_text='The date and time the review was last edited.')
+    creator =  models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, help_text='The Bok that tis review is for.')
