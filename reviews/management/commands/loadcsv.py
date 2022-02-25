e734f7c67271554619fb41d770e2bc29b3e5ccae
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-25 17:36:30 -0300

    include row_to_dict static method

--- reviews/management/commands/loadcsv.py
+++ reviews/management/commands/loadcsv.py
@@ -10,3 +10,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--csv', type=str)
+
+    @staticmethod 
+    def row_to_dict(row, header):
+        if len(row) < len(header):
+            row += [''] * (len(header) - len(row))
+        return dict([(header[i], row[i]) for i, head in enumerate(header) if head])
