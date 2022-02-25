7eb525fa99950ab2547fa2e209bee285d8a722f4
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-25 17:32:43 -0300

    include add_arguments method into Command class

--- reviews/management/commands/loadcsv.py
+++ reviews/management/commands/loadcsv.py
@@ -4,3 +4,9 @@ import re
 from django.contrib.auth.models import User 
 from django.core.management.base import BaseCommand, CommandError 
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
+
+class Command(BaseCommand):
+    help = 'Load the reviews data from a CSV file.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--csv', type=str)
