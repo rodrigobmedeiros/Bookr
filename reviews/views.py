b1e1030955bf5937567794b49ebdde4e892f07fa
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-27 13:33:50 -0300

    change views to render the new template base.html

--- reviews/views.py
+++ reviews/views.py
@@ -5,10 +5,7 @@ from .models import Book
 
 def welcome_view(request):
 
-    message = f'<html><h1>Welcome to Bookr!</h1>'\
-        f'<p>{Book.objects.count()} books and counting!</p></html>'
-
-    return HttpResponse(message)
+    return render(request, 'base.html')
 
 def index(request):
 
