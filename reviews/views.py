86eda9a535e852270e76b7b45669cf7438d6dc1d
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-26 23:07:30 -0300

    include welcome view with HttpResponse

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Book
+
+def welcome_view(request):
+
+    message = f'<html><h1>Welcome to Bookr!</h1>'\
+        '<p>{Book.objects.count()} books and counting!</p></html>'
+
+    return HttpResponse(message)
 
 def index(request):
 
