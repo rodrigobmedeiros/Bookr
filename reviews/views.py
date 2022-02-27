1422b23a3b180c5e122a51b5847349e551cc19eb
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-02-27 13:27:59 -0300

    create templates folder and add it to settings.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Book
+from .models import Book
 
 def welcome_view(request):
 
     message = f'<html><h1>Welcome to Bookr!</h1>'\
-        '<p>{Book.objects.count()} books and counting!</p></html>'
+        f'<p>{Book.objects.count()} books and counting!</p></html>'
 
     return HttpResponse(message)
 
