6a16553aaf53acfbd28d2cf7c72da8ca80214189
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-01 22:41:00 -0300

    create a view to get all books and return ordered information

--- reviews/views.py
+++ reviews/views.py
@@ -7,6 +7,14 @@ def welcome_view(request):
 
     return render(request, 'base.html')
 
+def books_list(request):
+
+    books = Book.objects.all()
+
+    for book in books:
+
+        # get all ratings for each book
+
 def index(request):
 
     return render(request, 'base.html')
