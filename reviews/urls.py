4f64ba27a946f783370e6d11a1cd94b06d9ca41b
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-04 15:34:23 -0300

    create a url directing to book_list view

--- reviews/urls.py
+++ reviews/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.welcome_view, name='welcome_view'),
+    path('books/', views.book_list, name='book_list'),
 ]
