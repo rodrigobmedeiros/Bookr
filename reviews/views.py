7834d0aca86cec4b3a1f9fd0c3dc7eb6170e0748
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-04 11:20:17 -0300

    creation of a view to return a list of books with its rating and number of reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,19 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
+from models import Book
+from bookr.utils import average_rating
 
-def welcome_view(request):
-
-    return render(request, 'base.html')
-
-def books_list(request):
+def book_list(request):
 
     books = Book.objects.all()
+    book_list = []
 
     for book in books:
 
-        # get all ratings for each book
-        book.title
-        
-    return render(request, 'base.html')
+        reviews = book.review_set.all()
+
+        book_rating = average_rating([
+            review.rating 
+            for review in reviews
+        ])
+
+        number_of_reviews = len(reviews)
+
+        book_list.append(
+            {
+                'book': book, 
+                'book_rating': book_rating,
+                'number_of_reviews': number_of_reviews
+            }
+        )
+
+    context = {'book_list': book_list}
+
+    return render(request, 'reviews.book_list.html')
