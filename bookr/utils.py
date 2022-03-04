1b5282538d7660bfe8232e06478131514f6bcd3e
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-04 16:31:13 -0300

    change return to 0 when there is no reviews

--- bookr/utils.py
+++
@@ -1,8 +0,0 @@
-def average_rating(rating_list):
-    """Calculate the average rating of a Book."""
-
-    if not rating_list:
-
-        return 0
-
-    return round(sum(rating_list) / len(rating_list))
