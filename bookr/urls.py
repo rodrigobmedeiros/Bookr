cdf54660b453f8ceb6815265689dc4a269aac2d4
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-14 21:26:28 -0300

    after finish configuration refer to admin site with admin.site

--- bookr/urls.py
+++ bookr/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from reviews.admin import admin_site
-
 urlpatterns = [
-    path('admin/', admin_site.urls),
+    path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
 ]
