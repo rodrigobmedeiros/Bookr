3ea1d8ef514efe1ae137be8432b448835c86b155
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-14 21:48:16 -0300

    import admin from django.contrib

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,9 +1,9 @@
-from django import contrib
+from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
 # Register your models here.
-contrib.admin.site.register(Publisher)
-contrib.admin.site.register(Contributor)
-contrib.admin.site.register(Book)
-contrib.admin.site.register(BookContributor)
-contrib.admin.site.register(Review)
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(BookContributor)
+admin.site.register(Review)
