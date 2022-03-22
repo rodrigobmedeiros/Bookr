1987cb8fcba3353c5446cac9728598f18fccc621
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-03-21 21:01:26 -0300

    add main route to the application

--- reviews/urls.py
+++ reviews/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:id>', views.book_detail, name='book_details'),
+    path('', views.main, name='main')
 ]
