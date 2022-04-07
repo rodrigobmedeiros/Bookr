e05eab87f5ac6bd2a086859594ec47fb249c1fa3
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-04-06 23:22:00 -0300

    adding comments to the code

--- reviews/views.py
+++ reviews/views.py
@@ -45,9 +45,11 @@ def book_detail(request, id):
 
 def book_search(request):
 
+    # To start the view, default values were defined for title and books
     title = 'Book Search'
     books = []
     
+    # To avoid DRY, search form is defined at this point
     if request.method == 'GET':
 
         search_form = SearchForm(request.GET)
@@ -56,8 +58,10 @@ def book_search(request):
 
         search_form = SearchForm(request.POST)
 
+    # Test if the form is valid
     if search_form.is_valid():
 
+        # Get values need to make searchs
         search_text = search_form.cleaned_data.get('search')
         search_in = search_form.cleaned_data.get('search_in')
 
@@ -65,15 +69,23 @@ def book_search(request):
 
             title = f'Search Results for "{search_text}"'
 
+            # Here two cases are covered, when info come from url (GET method).
+            # In this case search_in is ''
+            # If submit button is clicked from HTML form (POST method), search_in became title or contributor
             if search_in in ['title', '']:
 
+                # Simple query based com title
                 books = Book.objects.filter(title__icontains=search_text)
 
             elif search_in == 'contributor':
 
+                # When contributor is selected, the search is executed based on two fields
+                # first_names and last_names, to perform that, django.db.models.Q was used
+                # To know more about that go to the link -> https://docs.djangoproject.com/en/4.0/topics/db/queries/#complex-lookups-with-q 
                 complex_search_query = Q(first_names__icontains=search_text) | Q(last_names__icontains=search_text)
                 contributors = Contributor.objects.filter(complex_search_query)
 
+                # once I have all contributors, It's possible to get all books from them
                 _ = [books.extend(contributor.book_set.all()) for contributor in contributors]
                 
     context = {
@@ -83,8 +95,6 @@ def book_search(request):
     }
 
     return render(request, 'reviews/book_search.html', context=context)
-
-
 
 def main(request):
 
