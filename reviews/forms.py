79511d522e2b4cc847c81945a97b3fc4cb50c692
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-04-12 21:30:58 -0300

    define PublisherForm class

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Publisher
 
 SEARCH_CHOICES = (
     ('title', 'title'),
@@ -9,3 +10,9 @@ class SearchForm(forms.Form):
 
     search = forms.CharField(min_length=3, required=False)
     search_in = forms.ChoiceField(choices=SEARCH_CHOICES, required=False)
+
+class PublisherForm(forms.ModelForm):
+
+    class Meta:
+        model = Publisher
+        fields = "__all__"
