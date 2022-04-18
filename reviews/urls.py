33b9ae59046cf404242fe21d891c9263cbab132a
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-04-18 10:36:00 -0300

    change urls to create and edit reviews based on a specific book

--- reviews/urls.py
+++ reviews/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('book_search/', views.book_search, name='book_search'),
     path('publishers/new/', views.publisher_edit, name='publisher_create'),
     path('publishers/<int:pk>/', views.publisher_edit, name='publisher_edit'),
-    path('reviews/new/', views.review_edit, name='review_create'),
+    path('books/<int:book_pk>/reviews/new/', views.review_edit, name='review_create'),
+    path('books/<int:book_pk>/reviews/<int:review_pk>', views.review_edit, name='review_edit'),
     path('', views.main, name='main')
 ]
