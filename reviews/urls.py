27c56d876a28bdc6d79e0af50c73ae4757a964b9
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-05-02 21:15:57 -0300

    redefine url for book meida

--- reviews/urls.py
+++ reviews/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('publishers/<int:pk>/', views.publisher_edit, name='publisher_edit'),
     path('books/<int:book_pk>/reviews/new/', views.review_edit, name='review_create'),
     path('books/<int:book_pk>/reviews/<int:review_pk>', views.review_edit, name='review_edit'),
-    path('books/<int:book_pk>/example', views.book_media, name='example'),
+    path('books/<int:book_pk>/media', views.book_media, name='book_media'),
     path('', views.main, name='main')
 ]
