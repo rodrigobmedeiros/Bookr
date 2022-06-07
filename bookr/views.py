abeac9322617b5a95e47890a79e205f3ef53996f
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-06-06 22:04:48 -0300

    add login_required decorator to profile function

--- bookr/views.py
+++ bookr/views.py
@@ -1,6 +1,8 @@
 import re
-from django.shortcuts import render 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def profile(request):
 
     return render(request, template_name='profile.html')
