0b3e52da5b5d8d4d78a57add0657a159ab2ec3b7
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-12 11:35:54 -0300

    add function to decode base64 session key and data

--- reviews/utils.py
+++ reviews/utils.py
@@ -1,3 +1,15 @@
+import base64
+import json
+import pprint
+import sys
+
+def get_session_dictionary(session_key):
+    """Function used to decode and get dictionary session."""
+
+    _, payload = base64.b64decode(session_key).split(b':', 1)
+    session_dictionary = json.loads(payload.decode())
+    return session_dictionary
+
 def average_rating(rating_list):
     """Calculate the average rating of a Book."""
 
@@ -6,3 +18,10 @@ def average_rating(rating_list):
         return 0
 
     return round(sum(rating_list) / len(rating_list))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        session_key = sys.argv[1]
+        session_dictionary = get_session_dictionary(session_key)
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(session_dictionary)
