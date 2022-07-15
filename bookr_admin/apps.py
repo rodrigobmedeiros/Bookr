09a8a3760e30d692cff5eb108e6ec20ccaa4b1fb
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-15 00:58:10 -0300

    Configuration of custom Admin Site to be the default one

--- bookr_admin/apps.py
+++ bookr_admin/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.contrib.admin.apps import AdminConfig
+
+import bookr_admin
 
 
 class BookrAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookr_admin'
+
+class BookrAdminConfig(AdminConfig):
+    default_site = 'bookr_admin.admin.BookrAdmin'
