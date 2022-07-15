c804d076da18575f36a146e45c6d077056db90bc
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-15 00:57:32 -0300

    remove user registration into custonm admin site

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 
 # Register your models here.
 class BookrAdmin(admin.AdminSite):
@@ -7,4 +6,3 @@ class BookrAdmin(admin.AdminSite):
 
 
 admin_site = BookrAdmin(name='bookr_admin')
-admin_site.register(User)
