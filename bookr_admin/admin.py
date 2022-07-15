e6242e1e53b0220cf165b29eb80787bb11b81cc3
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-15 00:33:27 -0300

    update site_header

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Register your models here.
 class BookrAdmin(admin.AdminSite):
-    site_header: str = 'Bookr Administration Rodrigo'
+    site_header: str = 'Bookr Administration'
 
 
 admin_site = BookrAdmin(name='bookr_admin')
