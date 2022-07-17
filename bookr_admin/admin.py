245b438996d14b2e874b7f0456ae7fa11822eb9a
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-17 12:13:47 -0300

    include profile view, with all needed to work - include into url patterns

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,9 +1,32 @@
+from django.template.response import TemplateResponse
 from django.contrib import admin
+from django.http import HttpRequest
+from django.urls  import path
 
 # Register your models here.
 class BookrAdmin(admin.AdminSite):
     site_header:str = 'Bookr Administration'
     logout_template:str = 'admin/logout.html'
+
+    def get_urls(self):
+        # get all predefined urls.
+        base_urls = super().get_urls()
+
+        # create urls for custom views
+        url_patterns = [
+            path("admin_profile/", self.profile_view)
+        ]
+
+        # put everuthing together to return as a complete list os urls patterns.
+        return base_urls + url_patterns
+
+    # example method to create a custom view to admin site
+    def profile_view(self, request:  HttpRequest):
+
+        request.current_app = self.name 
+        context = self.each_context(request)
+
+        return TemplateResponse(request, "admin/admin_profile.html", context=context)
 
 
 admin_site = BookrAdmin(name='bookr_admin')
