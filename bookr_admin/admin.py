fcc61ffae19519e806181b69b75b164f0dc973cb
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-18 18:23:33 -0300

    change url order to define custom urls for admin site

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,6 +1,5 @@
 from django.template.response import TemplateResponse
 from django.contrib import admin
-from django.http import HttpRequest
 from django.urls  import path
 
 # Register your models here.
@@ -12,23 +11,16 @@ class BookrAdminCustom(admin.AdminSite):
         # get all predefined urls.
         base_urls = super().get_urls()
 
-        # create urls for custom views
         url_patterns = [
-            path("admin_profile", self.admin_view(self.profile_view))
-        ]
+            path("admin_profile", self.admin_view(self.profile_view)),  
+        ] + base_urls
 
-        # put everuthing together to return as a complete list os urls patterns.
-        return base_urls + url_patterns
+        # put everything together to return as a complete list os urls patterns.
+        return url_patterns
 
     # example method to create a custom view to admin site
-    def profile_view(self, request:  HttpRequest):
-
+    def profile_view(self, request):
         request.current_app = self.name 
         context = self.each_context(request)
 
         return TemplateResponse(request, "admin/admin_profile.html", context=context)
-
-    
-
-
-admin_site = BookrAdminCustom(name='bookr_admin')
