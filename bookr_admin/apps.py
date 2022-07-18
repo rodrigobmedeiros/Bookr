e6990084778d570ed6b651b119e09ab10733d698
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-18 18:23:55 -0300

    change class names

--- bookr_admin/apps.py
+++ bookr_admin/apps.py
@@ -1,9 +1,6 @@
 from django.apps import AppConfig
 from django.contrib.admin.apps import AdminConfig
 
-import bookr_admin
-
-
 class BookrAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookr_admin'
