3445857a7be85647db8dde69f6490c58c3927a74
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-21 07:02:34 -0300

    Create an app to implement and test class based views

---
+++ book_management/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.edit import FormView
+
+from .forms import BookForm
+
+
+# Create your views here.
+class BookRecordFormView(FormView):
+
+    template_name = 'book_form.html'
+    form_class = BookForm
+    success_url = '/book_management/entry_success'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+class FormSuccessView(View):
+
+    def get(self, request, *args,  **Kwargs):
+        return HttpResponse("Book record saved successfully")
+
