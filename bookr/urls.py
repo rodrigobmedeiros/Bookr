3160f33ca3951969ebc4e6d9289d3bf7bd15d075
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-21 07:03:36 -0300

    map urls for bookr_management app

--- bookr/urls.py
+++ bookr/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('', include('reviews.urls')),
     path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('accounts/profile/', profile, name='profile'),
-    path('filter_demo/', include('filter_demo.urls'))
+    path('filter_demo/', include('filter_demo.urls')),
+    path('book_management/', include('book_management.urls'))
+
 ]
 
 if settings.DEBUG:
