6c6d647800b7093abb6c45f9bf49d8d64fa12ed6
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-07-22 07:14:40 -0300

    include a new view to insert new books into database

--- book_management/views.py
+++ book_management/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import BookForm
+from .models import Book
 
 
 # Create your views here.
@@ -21,4 +22,13 @@ class FormSuccessView(View):
 
     def get(self, request, *args,  **Kwargs):
         return HttpResponse("Book record saved successfully")
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = [
+        'name',
+        'author'
+    ]
+    template_name = 'book_form.html'
+    success_url = '/book_management/entry_success'
 
