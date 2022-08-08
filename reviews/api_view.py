3fcf687d808ee8da5809d9f12bdd28027bbb6348
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-08-08 20:40:03 -0300

    include a class-based approach to get a list of all books

--- reviews/api_view.py
+++ reviews/api_view.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
+
 from .models import Book, Publisher 
 from .serializers import BookSerializer, PublisherSerializer
 
@@ -9,3 +11,7 @@ def all_books(request):
     books = Book.objects.all()
     book_serializer = BookSerializer(books, many=True)
     return Response(book_serializer.data)
+
+class AllBooks(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
