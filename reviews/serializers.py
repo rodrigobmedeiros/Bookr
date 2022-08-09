cfe5c02da07e220a51b2a1d7bd0536335883c7ed
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-08-09 10:58:14 -0300

    include BookContributor and Contributor Serializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Contributor, Publisher, Book
+from .models import Contributor, Publisher, Book, BookContributor
 
 class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,4 +17,29 @@ class BookSerializer(serializers.ModelSerializer):
             'publication_date',
             'isbn',
             'publisher'
+        ]
+
+class BookContributorSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+    
+    class Meta:
+        model = BookContributor
+        fields = [
+            'book',
+            'role'
+        ]
+        
+
+class ContributorSerializer(serializers.ModelSerializer):
+
+    contributions = BookContributorSerializer(source='bookcontributor_set', many=True)
+
+    class Meta:
+        model = Contributor
+        fields = [
+            'first_names',
+            'last_names',
+            'email',
+            'number_of_contributions',
+            'contributions'
         ]
